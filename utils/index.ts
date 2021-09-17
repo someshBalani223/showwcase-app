0716cea48281a884e2f620e56a01ad075e74be65
Somesh <sbalani@deqode.com>
2021-09-17 18:44:38 +0530

    Added utils

---
+++ utils/index.ts
@@ -0,0 +1,18 @@
+export const getMonthAndYear = (input: string) => {
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const date = new Date(input);
+  return monthNames[date.getMonth()] + ' '  + date.getFullYear();
+};
